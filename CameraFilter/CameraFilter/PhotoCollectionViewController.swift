ad67d99d52d74ef22e3e74c6294f28fd2f1c8e61
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-03-08 21:46:14 +0200

    Setting Up - Camera Filter App - Commit

---
+++ CameraFilter/CameraFilter/PhotoCollectionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCollectionViewController.swift
+//  CameraFilter
+//
+//  Created by Ahmed Salem on 08/03/2023.
+//
+
+import UIKit
+
+
+
+class PhotoCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+       
+
+        
+    }
+
+
+
+}
---
+++ CameraFilter/CameraFilter/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CameraFilter
+//
+//  Created by Ahmed Salem on 08/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Camera Filter"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+
+
+}
+
