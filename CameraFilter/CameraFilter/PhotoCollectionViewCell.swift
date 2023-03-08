a3e5895665734385b8c36960267fe5b4c5a1ccd9
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-03-08 22:57:45 +0200

    Displaying Photos From Photo Library - Commit

---
+++ CameraFilter/CameraFilter/PhotoCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilter
+//
+//  Created by Ahmed Salem on 08/03/2023.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView :UIImageView!
+}
