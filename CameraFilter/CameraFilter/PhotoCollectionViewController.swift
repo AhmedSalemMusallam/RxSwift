d7102da62b4a08ad3f58bcf743bbfb773363f3a7
Ahmed Salem Musallam Ali <117483832+AhmedSalemMusallam@users.noreply.github.com>
2023-03-08 22:24:50 +0200

    Fetching All Photo Assets - Commit

--- CameraFilter/CameraFilter/PhotoCollectionViewController.swift
+++ CameraFilter/CameraFilter/PhotoCollectionViewController.swift
@@ -11,6 +11,8 @@ import Photos
 
 class PhotoCollectionViewController: UICollectionViewController {
 
+    private var images = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,10 +25,18 @@ class PhotoCollectionViewController: UICollectionViewController {
     
     private func populatePhotos()
     {
-        PHPhotoLibrary.requestAuthorization{ status in
+        PHPhotoLibrary.requestAuthorization{ [weak self] status in
             if status == .authorized
             {
-                // access the photos from library 
+                // access the photos from library
+                let assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: nil)
+                assets.enumerateObjects{ (object, count, stop) in
+                    self?.images.append(object)
+                }
+                
+                self?.images.reverse()
+                print(self?.images)
+//                self?.collectionView.reloadData()
             }
             
         }
